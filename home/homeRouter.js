dd8b2fda2f2b61cf4e4738b3e104a9b3b7b37cb7
An Nguyen <ducan172002@gmail.com>
2022-11-27 01:58:01 +0700

    up data admin

--- home/homeRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const homeController = require('../home/homeController')
-/* GET users listing. */
-router.get('/', homeController.getPartner);
-
-module.exports = router;
