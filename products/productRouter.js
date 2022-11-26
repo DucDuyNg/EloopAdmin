4e942e752406b881a1c2ad0216df28876c3745d7
DuyAlann <datnguyen180502@gmail.com>
2022-11-26 18:33:33 +0700

    Eloop

---
+++ products/productRouter.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const productController = require('../products/productController')
+
+router.get('/', productController.getListProduct);
+router.get('/:expore', productController.getListProduct);
+router.get('/detail/:id', productController.getProductDetail);
+
+
+
+
+module.exports = router;
