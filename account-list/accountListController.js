f430ff22144efb62e1952e806ff4da64c33fedb1
DuyAlann <datnguyen180502@gmail.com>
2022-12-14 16:13:56 +0700

    View user account list

--- account-list/accountListController.js
+++ account-list/accountListController.js
@@ -1,7 +1,7 @@
 const {user,findUser,findAllUser,addUser,removeUser,updateUser,getAccountList} = require('../models/user')
 exports.getAccountList = async (req,res,next) =>{
 
-      const accountList = await getAccountListproduct();
+      const accountList = await getAccountList();
       console.log(accountList.length);
   
       res.render('admins/account-list', {accountList : accountList})
